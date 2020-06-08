6de73b36b6d5938c28417c9db6ceff99bf3d02c5
Lenuward <66222295+Lenuward@users.noreply.github.com>
2020-06-09 00:40:36 +0800

    Add files via upload

---
+++ xx.js
@@ -0,0 +1,8 @@
+function toggleSlidebar(){
+	if(document.getElementById('wrapper').classList.toggle('xx')==true){
+			document.getElementById("ss").innerHTML = "&#8810;"
+	}else{
+			document.getElementById("ss").innerHTML = "&#8811;"
+
+	}
+}
