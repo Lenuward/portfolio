2288684eaef3c7897dc6d75a0c641d457fcd118d
lenuward <lestuista55@yahoo.com>
2023-12-03 23:55:38 +0800

    Changed colors and design

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,13 @@
-const home = document.getElementById("home"),
-	home2 = document.getElementById("home2"),
-	about = document.getElementById("about"),
-	skills = document.getElementById("skills"),
-	works = document.getElementById("works"),
-	contact = document.getElementById("contact"),
-	home3 = document.getElementById("home3"),
-	hiddenElm = document.querySelectorAll(".hidden"),
-	hiddenlefttoright = document.querySelectorAll(".hiddenlefttoright"),
-	hiddenrighttoleft = document.querySelectorAll(".hiddenrighttoleft");
+const home = document.getElementById("home");
+const home2 = document.getElementById("home2");
+const about = document.getElementById("about");
+const skills = document.getElementById("skills");
+const works = document.getElementById("works");
+const contact = document.getElementById("contact");
+const home3 = document.getElementById("home3");
+const hiddenElm = document.querySelectorAll(".hidden");
+const hiddenlefttoright = document.querySelectorAll(".hiddenlefttoright");
+const hiddenrighttoleft = document.querySelectorAll(".hiddenrighttoleft");
 let options = {
 	root: null,
 	rootMargin: "0px",
@@ -18,52 +18,89 @@ const observer = new IntersectionObserver((e, o) => {
 		e.isIntersecting ? e.target.classList.add("show") : e.target.classList.remove("show")
 	})
 });
-
-hiddenlefttoright.forEach(e => observer.observe(e)), 
-hiddenrighttoleft.forEach(e => observer.observe(e)), 
-hiddenElm.forEach(e => observer.observe(e)), 
+function isNavItemActive(what){
+    var b = document.getElementsByClassName('nav-item');
+    for(var a = 0; a<b.length;a++){
+        if(what == "home"){
+            b[a].classList.remove('active-border');
+            b[a].classList.add('inactive-border');
+            b[0].classList.add('active-border');
+            b[0].classList.remove('inactive-border');
+        }else if(what == "about"){
+            b[a].classList.remove('active-border');
+            b[a].classList.add('inactive-border');
+            b[1].classList.add('active-border');
+            b[1].classList.remove('inactive-border');
+        }else if(what == "skills"){
+            b[a].classList.remove('active-border');
+            b[a].classList.add('inactive-border');
+            b[2].classList.add('active-border');
+            b[2].classList.remove('inactive-border');
+        }else if(what == "works"){
+            b[a].classList.remove('active-border');
+            b[a].classList.add('inactive-border');
+            b[3].classList.add('active-border');
+            b[3].classList.remove('inactive-border');
+        }else if(what == "contact"){
+            b[a].classList.remove('active-border');
+            b[a].classList.add('inactive-border');
+            b[4].classList.add('active-border');
+            b[4].classList.remove('inactive-border');
+        }
+    }
+}
+hiddenlefttoright.forEach(e => observer.observe(e));
+hiddenrighttoleft.forEach(e => observer.observe(e));
+hiddenElm.forEach(e => observer.observe(e));
 home.onclick = function() {
-	hiddenlefttoright.forEach(e => observer.observe(e)), 
-    hiddenrighttoleft.forEach(e => observer.observe(e)), 
-    hiddenElm.forEach(e => observer.observe(e)), 
-    document.body.scrollIntoView()
+    isNavItemActive("home");
+	hiddenlefttoright.forEach(e => observer.observe(e));
+    hiddenrighttoleft.forEach(e => observer.observe(e));
+    hiddenElm.forEach(e => observer.observe(e));
+    document.body.scrollIntoView();
 }, 
 home2.onclick = function() {
-	hiddenlefttoright.forEach(e => observer.observe(e)), 
-    hiddenrighttoleft.forEach(e => observer.observe(e)), 
-    hiddenElm.forEach(e => observer.observe(e)), 
-    document.getElementById("secondpage").scrollIntoView()
+    isNavItemActive("about");
+	hiddenlefttoright.forEach(e => observer.observe(e));
+    hiddenrighttoleft.forEach(e => observer.observe(e));
+    hiddenElm.forEach(e => observer.observe(e));
+    document.getElementById("secondpage").scrollIntoView();
 }, 
 home3.onclick = function() {
-	hiddenlefttoright.forEach(e => observer.observe(e)), 
-    hiddenrighttoleft.forEach(e => observer.observe(e)), 
-    hiddenElm.forEach(e => observer.observe(e)), 
-    document.getElementById("firstpage").scrollIntoView()
+	hiddenlefttoright.forEach(e => observer.observe(e));
+    hiddenrighttoleft.forEach(e => observer.observe(e));
+    hiddenElm.forEach(e => observer.observe(e));
+    document.getElementById("firstpage").scrollIntoView();
 }, 
 about.onclick = function() {
-	hiddenlefttoright.forEach(e => observer.observe(e)), 
-    hiddenrighttoleft.forEach(e => observer.observe(e)), 
-    hiddenElm.forEach(e => observer.observe(e)), 
-    document.getElementById("secondpage").scrollIntoView()
+    isNavItemActive("about");
+	hiddenlefttoright.forEach(e => observer.observe(e));
+    hiddenrighttoleft.forEach(e => observer.observe(e));
+    hiddenElm.forEach(e => observer.observe(e));
+    document.getElementById("secondpage").scrollIntoView();
 }, 
 skills.onclick = function() {
-	hiddenlefttoright.forEach(e => observer.observe(e)), 
-    hiddenrighttoleft.forEach(e => observer.observe(e)), 
-    hiddenElm.forEach(e => observer.observe(e)), 
-    document.getElementById("thirdpage").scrollIntoView()
+    isNavItemActive("skills");
+	hiddenlefttoright.forEach(e => observer.observe(e));
+    hiddenrighttoleft.forEach(e => observer.observe(e));
+    hiddenElm.forEach(e => observer.observe(e));
+    document.getElementById("thirdpage").scrollIntoView();
 }, 
 works.onclick = function() {
-	hiddenlefttoright.forEach(e => observer.observe(e)), 
-    hiddenrighttoleft.forEach(e => observer.observe(e)), 
-    hiddenElm.forEach(e => observer.observe(e)), 
-    document.getElementById("fourthpage").scrollIntoView()
+    isNavItemActive("works");
+	hiddenlefttoright.forEach(e => observer.observe(e));
+    hiddenrighttoleft.forEach(e => observer.observe(e));
+    hiddenElm.forEach(e => observer.observe(e));
+    document.getElementById("fourthpage").scrollIntoView();
 }, 
 contact.onclick = function() {
-	hiddenlefttoright.forEach(e => observer.observe(e)), 
-    hiddenrighttoleft.forEach(e => observer.observe(e)), 
-    hiddenElm.forEach(e => observer.observe(e)), 
-    document.getElementById("fifthpage").scrollIntoView()
+    isNavItemActive("contact");
+	hiddenlefttoright.forEach(e => observer.observe(e));
+    hiddenrighttoleft.forEach(e => observer.observe(e));
+    hiddenElm.forEach(e => observer.observe(e));
+    document.getElementById("fifthpage").scrollIntoView();
 }, 
+document.getElementById("")
 document.body.onload = function() {
     setInterval(displayHello, 20000);
     function displayHello() {
@@ -72,12 +109,12 @@ document.body.onload = function() {
             element[a].classList.toggle("hideIt");
         }
     }
-	$("#target").clearIt().typeIt("Lenbnard Estuisat", .06, "text").pauseIt(1).deleteIt(33, .09).typeIt("Lenarnd Estiujsa", .05, "text").pauseIt(1).deleteIt(33, .09).typeIt("Lennard Estuista", .05, "text").hideCursor()
+	$("#target").clearIt().typeIt("Lenbnard Estuisat", .06, "text").pauseIt(1).deleteIt(33, .09).typeIt("Lenarnd Estiujsa", .05, "text").pauseIt(1).deleteIt(33, .09).typeIt("Lennard Estuista", .05, "text").hideCursor();
 };
 
 document.getElementById('contactSubmit').onsubmit = function(e) {
 	e.preventDefault();
-	alert('This don\'t have function yet. Sorry!')
+	alert('This don\'t have function yet. Sorry!');
 };
 $('.navbar-nav>li>a').on('click', function() {
 	$('.navbar-collapse').collapse('hide');
